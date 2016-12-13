35cfdb73e841ee865b2023b5a4bd527e58f8c2ad
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2016-12-13 23:57:12 +0100

    Show errors when wrap tests fail.

--- test/wrap.js
+++ test/wrap.js
@@ -1,32 +1,49 @@
 var test = require('tape');
 var path = require('path');
-var exec = require('child_process').exec;
+var spawnSync = require('child_process').spawnSync;
 
 var bin = path.resolve('bin', 'colortape');
 
 test('wrap: exit code 0 when success', function(t) {
   t.plan(1);
   var fixturePath = path.resolve('test', 'fixtures', 'success.js');
-  var child = exec(['node', bin, fixturePath].join(' '));
-  child.on('exit', function(code) {
-    t.equal(code, 0);
-  });
+
+  var result = spawnSync('node', [bin, fixturePath]);
+
+  if (result.status !== 0) {
+    printOutput(result);
+  }
+  t.equals(result.status, 0);
 });
 
 test('wrap: exit code non-zero when failure', function(t) {
   t.plan(1);
   var fixturePath = path.resolve('test', 'fixtures', 'failure.js');
-  var child = exec(['node', bin, fixturePath].join(' '));
-  child.on('exit', function(code) {
-    t.notEqual(code, 0);
-  });
+
+  var result = spawnSync('node', [bin, fixturePath]);
+
+  if (result.status === 0) {
+    printOutput(result);
+  }
+  t.notEqual(result.status, 0);
 });
 
 test('wrap: exit code non-zero when error', function(t) {
   t.plan(1);
   var fixturePath = path.resolve('test', 'fixtures', 'error.js');
-  var child = exec(['node', bin, fixturePath].join(' '));
-  child.on('exit', function(code) {
-    t.notEqual(code, 0);
-  });
+
+  var result = spawnSync('node', [bin, fixturePath]);
+
+  if (result.status === 0) {
+    printOutput(result);
+  }
+  t.notEqual(result.status, 0);
 });
+
+function printOutput(result) {
+  console.log('----stdout----');
+  console.log(result.stdout);
+  console.log('----stderr----');
+  console.log(result.stderr);
+  console.log('--------------');
+}
